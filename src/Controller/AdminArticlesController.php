165d5c068d80d6c10e7fd163ca3c760a522026c0
LuckyShuii <lucasnimes30000@gmail.com>
2024-01-30 15:47:28 +0100

    added buttons articles filter by status - without style - missing method in abstract

--- src/Controller/AdminArticlesController.php
+++ src/Controller/AdminArticlesController.php
@@ -10,7 +10,20 @@ class AdminArticlesController extends AbstractController
     public function index(): string
     {
         $articleManager = new ArticleManager();
-        $articles = $articleManager->selectAll();
+
+        if (isset($_GET['status'])) {
+            $list = $_GET['status'];
+            if ($list === 'archived') {
+                $articles = $articleManager->selectByConditions('status', '3');
+            } elseif ($list === 'draft') {
+                $articles = $articleManager->selectByConditions('status', '1');
+            } else {
+                $articles = ['error' => 'Aucun article ne correspond à votre recherche'];
+            }
+        } else {
+            $articles = $articleManager->selectAll();
+        }
+
 
         return $this->twig->render('AdminArticles/index.html.twig', [
             'articles' => $articles

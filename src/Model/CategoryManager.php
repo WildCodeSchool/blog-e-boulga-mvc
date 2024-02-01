524795c803eefe786eae4f4a1a1c18382be518cb
Mono <quentinguillemineau@gmail.com>
2024-02-01 15:49:47 +0100

    use abstract to all categ method

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -11,8 +11,6 @@ class CategoryManager extends AbstractManager
 
     public function getAllCategory(): array
     {
-        $statement = $this->pdo->query('SELECT * FROM category ORDER BY categoryName ASC');
-        $statement->setFetchMode(PDO::FETCH_CLASS, static::CLASSNAME);
-        return $statement->fetchAll();
+        return $this->selectAll();
     }
 }

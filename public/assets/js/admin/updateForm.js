5c5cc13ebfe6c4ddd728cfa1fa121f12c73821da
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2024-02-06 16:02:02 +0100

    add js for new image in update form

--- public/assets/js/admin/updateForm.js
+++ public/assets/js/admin/updateForm.js
@@ -1,9 +1,17 @@
-function previewImage(event) {
-    var reader = new FileReader();
-    reader.onload = function(){
-        var output = document.getElementById('preview');
-        output.src = reader.result;
-    };
-    reader.readAsDataURL(event.target.files[0]);
+const imageUpload = document.getElementById('imageUpload');
+console.log(imageUpload+'imageUpload');
+function previewImage(e) {
+   const input = e.target;
+   const image = document.getElementById('preview');
+   
+   console.log(input.files);
+   if (input.files && input.files[0]) {
+      const reader = new FileReader();
+        reader.onload = function(e) {
+             image.src = e.target.result;
+        }
+        reader.readAsDataURL(input.files[0]);
+   }
 }
 
+document.getElementById('imageUpload').addEventListener('change', previewImage);

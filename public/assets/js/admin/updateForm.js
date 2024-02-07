189a3e029026a3cff80cc0a6c05446a5336d8bcb
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2024-02-07 14:57:02 +0100

    add css to form and change filename article

--- public/assets/js/admin/updateForm.js
+++ public/assets/js/admin/updateForm.js
@@ -1,10 +1,8 @@
 const imageUpload = document.getElementById('imageUpload');
-console.log(imageUpload+'imageUpload');
 function previewImage(e) {
    const input = e.target;
    const image = document.getElementById('preview');
    
-   console.log(input.files);
    if (input.files && input.files[0]) {
       const reader = new FileReader();
         reader.onload = function(e) {
@@ -13,5 +11,4 @@ function previewImage(e) {
         reader.readAsDataURL(input.files[0]);
    }
 }
-
 document.getElementById('imageUpload').addEventListener('change', previewImage);
